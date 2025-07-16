60dd4bf3734b15a9e97d8e650d8db82455d959e0
sbkashif <salmanbinkashif@gmail.com>
2025-07-16 11:44:16 -0400

    replace bits/stdc++ dependent libraries with those compatible with buffalo ccr

--- libraries.h
+++ libraries.h
@@ -6,8 +6,16 @@
 #include <cstring>
 #include <iomanip>
 #include <string> 
-#include <bits/stdc++.h>
+// Replaced <bits/stdc++.h> with specific headers it might be using
+#include <random>
+#include <ctime>
+#include <sstream>
+#include <limits>
+#include <map>
+#include <set>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>  // For access() function
+#include <cstdlib>   // For getenv() function
 #include <Eigen/Eigenvalues>
 using namespace std;
